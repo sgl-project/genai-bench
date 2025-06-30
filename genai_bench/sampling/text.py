aabb754dc5f6af821f7f965ea44d3fd7157f7326
Arthur Cheng <arthurcheng1991@outlook.com>
2025-06-30 14:01:35 -0700

    [BUG] Fix left tokens to sample in _sample_text (#20)

--- genai_bench/sampling/text.py
+++ genai_bench/sampling/text.py
@@ -10,6 +10,7 @@ from genai_bench.protocol import (
 )
 from genai_bench.sampling.base import Sampler
 from genai_bench.scenarios.base import EmbeddingDistribution, Scenario, TextDistribution
+from genai_bench.utils import calculate_char_token_ratio
 
 logger = init_logger(__name__)
 
@@ -38,6 +39,7 @@ class TextSampler(Sampler):
 
         self.data = data
         self.use_scenario = use_scenario
+        self.char_token_ratio = calculate_char_token_ratio(tokenizer, data)
 
         # Set ignore_eos based on scenario usage
         if use_scenario:
@@ -181,7 +183,7 @@ class TextSampler(Sampler):
                 if tokens > left_tokens_to_sample:
                     # This will cut off a line in the middle of a word, but
                     # that's ok since a llm should be able to handle that.
-                    prompt += line[:left_tokens_to_sample]
+                    prompt += line[: int(left_tokens_to_sample * self.char_token_ratio)]
                     return prompt
                 prompt += line
                 left_tokens_to_sample -= tokens
--- genai_bench/utils.py
+++ genai_bench/utils.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 
 from transformers import PreTrainedTokenizer
+from typing import List
 
 
 def sanitize_string(input_str: str):
@@ -32,15 +33,25 @@ def is_single_experiment_folder(folder_name: str) -> bool:
     return len(subfolders) == 0
 
 
+def calculate_char_token_ratio(
+    tokenizer: PreTrainedTokenizer,
+    data: str | List[str],
+    add_special_tokens: bool = False,
+) -> float:
+    """Calculate the ratio of character to token using model tokenizer."""
+    content = data if isinstance(data, str) else " ".join(data)
+    total_chars = len(content)
+    tokens = tokenizer.encode(content, add_special_tokens=add_special_tokens)
+    total_tokens = len(tokens)
+
+    char_token_ratio = total_chars / total_tokens if total_tokens > 0 else 0
+    return char_token_ratio
+
+
 def calculate_sonnet_char_token_ratio(tokenizer: PreTrainedTokenizer) -> float:
     """Calculate the ratio of character to token using model tokenizer."""
     sonnet_file = Path(__file__).parent.resolve() / "data/sonnet.txt"
     with open(sonnet_file, "r") as f:
         content = f.read()
 
-    total_chars = len(content)
-    tokens = tokenizer.encode(content, add_special_tokens=False)
-    total_tokens = len(tokens)
-
-    char_token_ratio = total_chars / total_tokens if total_tokens > 0 else 0
-    return char_token_ratio
+    return calculate_char_token_ratio(tokenizer, content, add_special_tokens=False)
