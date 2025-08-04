ea4085ee219351451f443077f68b175e52860f46
Chang Su <csu272@usc.edu>
2025-08-04 14:47:32 -0700

    bugfix: Fix typing in `ImageDatasetLoader` and num_images check (#53)

--- genai_bench/sampling/image.py
+++ genai_bench/sampling/image.py
@@ -62,7 +62,7 @@ class ImageSampler(Sampler):
         else:
             # Dataset-only mode
             prompt, image_content = self._sample_image_and_text()
-            num_images = 1
+            num_images = len(image_content)
             num_output_tokens = None
 
         # TODO: create Delegated Request Creator to replace if-else
